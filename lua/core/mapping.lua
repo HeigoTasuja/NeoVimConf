a0ca3c2e3c8c8943e2c2cbfdc7077ef90441e5c0
HeigoTasuja <97948830+HeigoTasuja@users.noreply.github.com>
2023-09-18 10:54:43 +0300

    Update mapping.lua

--- lua/core/mapping.lua
+++ lua/core/mapping.lua
@@ -1,2 +1,2 @@
-vim.g.mapleader = "-" --space before custom command
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) --so would not have to do :Ex every time
+vim.g.mapleader = " " --space before custom command
+vim.keymap.set("n", "<leader>ef", vim.cmd.Ex) --so would not have to do :Ex every time
