6543b0f6d7d2828f2452b6314ac0c1ae4300ba25
HeigoTasuja <97948830+HeigoTasuja@users.noreply.github.com>
2023-09-29 11:06:40 +0000

    color schema

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -5,7 +5,7 @@
 vim.opt.termguicolors = true
 
 function SetColor(color)
-    color = color or "jellybeans" -- have a default value
+    color = color or "gruvbox" -- have a default value
     vim.cmd.colorscheme(color)
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
